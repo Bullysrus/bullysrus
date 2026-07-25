9214d23b676db663e83e8611f22fa07de3521f59
Bullysrus <nataliecal313@gmail.com>
2026-07-24 21:11:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -89,15 +89,3 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 });
-const menuButton = document.querySelector(".menu-toggle");
-const nav = document.querySelector("nav");
-
-if(menuButton){
-
-menuButton.addEventListener("click", function(){
-
-nav.classList.toggle("active");
-
-});
-
-}
